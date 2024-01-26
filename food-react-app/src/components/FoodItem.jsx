49a1584ef4317c4d3626a86826e1483c4d561a23
Abhishek Govindaraj <abhishekgh3103@gmail.com>
2024-01-26 22:03:19 +0530

    Add style to foodItem component

--- food-react-app/src/components/FoodItem.jsx
+++ food-react-app/src/components/FoodItem.jsx
@@ -2,8 +2,13 @@ import styles from "./foodItem.module.css"
 export default function FoodItem({food}) {
     return (
         <div className={styles.itemDiv}>
-            <h2>{food.title}</h2>
-            <img src={food.image} alt={`${food.title} image`} />
+            <img className={styles.itemImage} src={food.image} alt={`${food.title} image`} />
+            <div className={styles.itemTitleContainer}>
+                <p className={styles.itemTitle}>{food.title}</p>
+            </div>
+            <div className={styles.itemMoreContainer}>
+                <button className={styles.itemButton}>View Recipe</button>
+            </div>
         </div>
     )
 }
